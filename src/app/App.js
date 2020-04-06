4abdd9dec421115a190ef1a1170c70cbfbd1ab4b
Jordan Kline <jordankline@Jordans-MacBook-Air.local>
2020-04-05 21:43:05 -0700

    Added properties and initial light state

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
 import { BrightnessSliderComponent } from "../components/BrightnessSlider/";
 import { ToggleSwitchComponent } from "../components/ToggleSwitch/";
+import * as Actions from "../Actions";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  constructor() {
+    super();
+
+    // initial state
+    this.state = {
+      on: undefined,
+      brightness: undefined,
+      name: undefined,
+      id: undefined,
+      error: undefined,
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      data: {
+        light: { on, brightness, name, id },
+      },
+    } = await Actions.getInitialState();
+
+    this.setState({
+      name,
+      id,
+      on,
+      brightness,
+    })
+
+    console.log(this.state)
+    return true;
+  }
+
   render() {
     return (
       <div className="App">
-        <BrightnessSliderComponent />
-        <ToggleSwitchComponent />
+        <h1>{this.state.name}</h1>
+        <ToggleSwitchComponent isLightOn={this.state.on}/>
+        <BrightnessSliderComponent currentBrightness={this.state.brightness}/>
       </div>
     );
   }
