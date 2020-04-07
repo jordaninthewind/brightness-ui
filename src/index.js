e555ad17a320e5a3f7cc102b58eace7673dae7ca
Jordan Kline <jordankline@Jordans-MacBook-Air.local>
2020-04-06 18:23:17 -0700

    Moved main container styles to root element

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./app/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div className="main-container">
+    <App />
+  </div>,
+  document.getElementById("root")
+);
